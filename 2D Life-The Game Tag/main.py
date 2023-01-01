aa3c5fa52746eeec6b40eb9d66ebebc65e27bc15
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2022-12-31 23:24:41 -0500

    MASSIVE BREAKTHROUGH
    I'm defining everything so that I don't have to put 100 lines of code into the game while loop.

--- 2D Life-The Game Tag/main.py
+++ 2D Life-The Game Tag/main.py
@@ -1,37 +1,6 @@
 import random, pygame, math, sys, time, buttoncode
 pygame.init()
 
-#button class
-class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
-	def draw(self, surface):
-		action = False
-		#get mouse position
-		pos = pygame.mouse.get_pos()
-		#check mouseover and clicked conditions
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
-				action = True
-		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
-		#draw button on screen
-		surface.blit(self.image, (self.rect.x, self.rect.y))
-		return action
-#button settings
-start_img = pygame.image.load('start_btn.png')
-exit_img = pygame.image.load('exit_btn.png')
-settings_img = pygame.image.load('options_btn.png')
-start_button = buttoncode.Button(650, 550, start_img, 0.4)
-exit_button = buttoncode.Button(650, 750, exit_img, 0.6)
-settings_button = buttoncode.Button(650, 650, settings_img, 0.6)
-
 def quit_game():
   pygame.quit()
   sys.exit()
@@ -62,6 +31,14 @@ blue = (0, 0, 255)
 white = (255, 255, 255)
 colors = [blue, red, green, white]
 
+#button settings
+start_img = pygame.image.load('start_btn.png')
+exit_img = pygame.image.load('exit_btn.png')
+settings_img = pygame.image.load('options_btn.png')
+start_button = buttoncode.Button(width/2.5, height/4.2, start_img, 1)
+exit_button = buttoncode.Button(width/2.5, height/1.8, exit_img, 1)
+settings_button = buttoncode.Button(width/2.5, height/2.5, settings_img, 1)
+
 #help dialogue
 help_dialogue = ("Press H for help | Press W, A, S, D or the arrow keys to move!")
 
@@ -69,7 +46,6 @@ help_dialogue = ("Press H for help | Press W, A, S, D or the arrow keys to move!
 font = pygame.font.Font('freesansbold.ttf', 38)
 title_font = pygame.font.Font('freesansbold.ttf', 79)
 title_intro_text_location = (width/6, height/25)
-singleplayer_intro_text_location = (width/20, height/6)
 multiplayer_intro_text_location = (width/20, height/4)
 settings_intro_text_location = (width/20, height/3)
 help_intro_text_location = (width/20, height/2.5)
@@ -83,7 +59,6 @@ top_right_four = (width/20, height/3.7)
 help_dialogue_text = font.render(help_dialogue, True, white)
 player_death_text = font.render('Player 1 Dead', True, white)
 title_intro_text = title_font.render('2D Life: The Game Tag', True, white)
-singleplayer_intro_text = font.render('Press O for Single Player', True, white)
 multiplayer_intro_text = font.render('Press P for Multi Player', True, white)
 settings_intro_text = font.render('Press S for Settings', True, white)
 help_intro_text = font.render('Press H for help', True, white)
@@ -100,19 +75,33 @@ player_x = width/2
 player_speed = 10
 
 #game stage settings
-game_stage = "intro" #game stage can = "intro", "settings", "singleplayer", and "multiplayer"
+def main_menu():
+  if start_button.draw(screen):
+    global game_stage
+    game_stage = "multiplayer"
+  if exit_button.draw(screen):
+    quit_game()
+  if settings_button.draw(screen):
+    game_stage = "settings"
+game_stage = "main_menu" #game stage can = "main_menu", "settings", and "multiplayer"
 
 print_settings_help = True
 
+#filling screen black variables
+main_menu_fill_black = True
+singleplayer_fill_black = True
+multiplayer_fill_black = True
+settings_fill_black = True
+
 running = True
 while running == True:
-  if game_stage == "intro":
-    start_button.draw(screen)
-    screen.blit(title_intro_text, title_intro_text_location)
-    screen.blit(singleplayer_intro_text, singleplayer_intro_text_location)
-    screen.blit(multiplayer_intro_text, multiplayer_intro_text_location)
-    screen.blit(settings_intro_text, settings_intro_text_location)
-    screen.blit(help_intro_text, help_intro_text_location)
+  if game_stage == "main_menu":
+    main_menu()
+    #screen.blit(title_intro_text, title_intro_text_location)
+    #screen.blit(singleplayer_intro_text, singleplayer_intro_text_location)
+    #screen.blit(multiplayer_intro_text, multiplayer_intro_text_location)
+    #screen.blit(settings_intro_text, settings_intro_text_location)
+    #screen.blit(help_intro_text, help_intro_text_location)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       quit_game()
@@ -127,13 +116,18 @@ while running == True:
         quit_game()
       if event.key == pygame.K_h:
         screen.blit(help_dialogue_text, help_dialogue_text_location)
+  #if start_button.draw(screen):
+  #  game_stage = "multiplayer"
   if game_stage == "settings":
+    if settings_fill_black == True:
+      screen.fill(black)
+    settings_fill_black = False
     size = width, height
     screen = pygame.display.set_mode(size)
     screen.fill(black)
     if print_settings_help == True:
       print("press J for 1000, 500 and N for 1500, 1000")
-      print_settings_help = False
+    print_settings_help = False
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_j:
@@ -142,30 +136,10 @@ while running == True:
         if event.key == pygame.K_n:
           width = 1500
           height = 1000
-  if game_stage == "singleplayer":
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_q:
-          game_stage = "intro"
-        if event.key == pygame.K_w:
-          pygame.draw.rect(screen, black, pygame.Rect(player_x, player_y, 50, 50))
-          player_y -= player_speed
-        if event.key == pygame.K_s:
-          pygame.draw.rect(screen, black, pygame.Rect(player_x, player_y, 50, 50))
-          player_y += player_speed
-        if event.key == pygame.K_a:
-          pygame.draw.rect(screen, black, pygame.Rect(player_x, player_y, 50, 50))
-          player_x -= player_speed
-        if event.key == pygame.K_d:
-          pygame.draw.rect(screen, black, pygame.Rect(player_x, player_y, 50, 50))
-          player_x += player_speed
-        if event.key == pygame.K_h:
-          screen.blit(help_dialogue_text, help_dialogue_text_location)
-        if event.key == pygame.K_ESCAPE:
-          quit_game()
-    pygame.draw.rect(screen, blue, pygame.Rect(player_x, player_y, 45, 45))
-    pygame.display.update()
   if game_stage == "multiplayer":
+    if multiplayer_fill_black == True:
+      screen.fill(black)
+      multiplayer_fill_black = False
     #rendering score onto screen in multiplayer mode
     score_text = font.render('Score: '+str(score), True, white)
     pygame.draw.rect(screen, black, pygame.Rect(width*0.01, height*0.9, 600, 50))
