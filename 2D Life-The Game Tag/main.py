c021fb31b5c236d4b7b77d9a5acdb7f81bfae052
RowenThompson <73656188+RowenThompson@users.noreply.github.com>
2023-01-12 22:53:37 -0500

    Small update
    Added player def and quit. Faced problems

--- 2D Life-The Game Tag/main.py
+++ 2D Life-The Game Tag/main.py
@@ -1,21 +1,35 @@
-import pygame
-screen_width = 1920
-screen_height = 1080
-
+import pygame, sys
 pygame.init()
-
+selff = "s"
+screen_width = 500
+screen_height = 500
+player_x = screen_width/2
+player_y = screen_height/2
 class MyGame():
-    def __init__(self, width, height):
-        self.screen_width = width
-        self.screen_height = height
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.player_x = player_x
+        self.player_y = player_y
+    def main():
+        """ Main function """
+        screen = MyGame(screen_width, screen_height)
+        display = pygame.display.set_mode((screen.screen_height, screen.screen_width))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                MyGame.quit_game()
+    def player(self):
+        self.player_x = player_x
+        self.player_y = player_y
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                MyGame.quit_game()
+    def quit_game():
+        pygame.quit()
+        sys.exit()
 
-def main():
-    """ Main function """
-    screen = MyGame(screen_width, screen_height)
-    
-    display = pygame.display.set_mode((screen.screen_height, screen.screen_width))
-    pygame.display.update()
-    display.fill((10,10,10))
 
 while __name__ == "__main__":
-    main() 
+    MyGame.main() 
+    MyGame.player(str(selff))
